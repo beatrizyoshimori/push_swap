c9c1d22aa6116d9c61bb1c2e5de5325fbc6797aa
Beatriz Yoshimori <beatrizyoshimori@MacBook-Air-de-Beatriz.local>
2023-03-19 19:38:33 -0300

    updated ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -14,29 +14,24 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	j;
+	int	sign;
 	int	num;
-
-	j = 0;
-	while ((nptr[j] >= 9 && nptr[j] <= 13) || nptr[j] == 32)
-		j++;
-	i = j;
-	if (nptr[i] != '-' && nptr[i] != '+' && ft_isdigit(nptr[i]) == 0)
-		return (0);
-	else if (nptr[i] == '-' || nptr[i] == '+')
+	
+	sign = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-')
 	{
-		if (ft_isdigit(nptr[i + 1]) != 1)
-			return (0);
-		i++;
+		sign = -1;
+		nptr++;
 	}
+	else if (*nptr == '+')
+		nptr++;
 	num = 0;
-	while (ft_isdigit(nptr[i]) == 1)
+	while (ft_isdigit(*nptr) == 1)
 	{
-		num = num * 10 + (nptr[i] - 48);
-		i++;
+		num = num * 10 + (*nptr - 48);
+		nptr++;
 	}
-	if (nptr[j] == '-')
-		num = -num;
-	return (num);
+	return (num * sign);
 }
